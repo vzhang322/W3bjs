2815b367595247e9b03fdcb673bc30ba04eeb42c
Vince Zhang <vincezrz584@gmail.com>
2023-09-26 00:29:49 +1300

    First coomit

---
+++ frontend/src/user/components/UserItem.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Avatar from "../../shared/component/UIElements/Avatar";
+import "./UserItem.css";
+
+
+const UserItem = props => {
+  return (
+    <li className="user-item">
+       <div className="user-item__content">
+         <div className="user-item__image">
+          <Avatar image={props.image} alt={props.name}/>
+         </div>
+         <div className="user-item__info">
+           <h2>{props.name}</h2>
+           <h3>
+              {props.placeCount} {props.placeCount === 1 ? 'Place' : 'Places'}
+           </h3>
+         </div>
+
+       </div>
+    </li>
+  );
+};
+
+export default UserItem;
